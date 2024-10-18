1d50ea9d824a0a5c94f5808c98b967c5b83fc09f
Guido Abel Sanchez <sanchezguidorl@gmail.com>
2024-10-18 17:19:16 -0300

    Se ajustan estilos de plato

--- app/src/infraestructure/components/pages/category/plates/CardPlate.tsx
+++ app/src/infraestructure/components/pages/category/plates/CardPlate.tsx
@@ -5,22 +5,20 @@ import { PlateEntity } from "@/app/src/domain/entity/Plate.entity";
 function CardPlate({ plateItem }: { plateItem: PlateEntity }) {
   return (
     <div className="h-24 mb-1 w-full overflow-hidden p-2 px-4 card-plates flex items-center">
-<div className=" overflow-hidden w-20 h-20 relative">
-<Image
-        width={150}
-        height={150}
-        className=" absolute object-cover w-full  h-full rounded-md"
-        src={plateItem.getImage()}
-        alt=""
-      />
-</div>
-      <div className=" pl-2 text-xs h-full relative text-black w-full">
+      <div className=" overflow-hidden flex justify-center items-center min-w-20 aspect-square relative">
+        <Image
+          width={150}
+          height={150}
+          className=" absolute object-cover w-full h-full rounded-md"
+          src={plateItem.getImage()}
+          alt=""
+        />
+      </div>
+      <div className=" pl-2 text-xs h-full  relative text-black w-full">
         <p className=" text-[0.93rem] pt-1 pr-7">{plateItem.getTitle()}</p>
         <p className=" flex bg-orange-300 z-10 h-5 px-[0.3rem] rounded-full font-bold w-10 gap-[0.12rem] justify-center items-center absolute top-1 -right-1 text-amber-950 font-sans text-[0.65rem] ">
           <Image className=" h-[15px] w-[15px]" src={StarIcon} alt="" />
-          <span>
-            {plateItem.getScore()}
-            </span>
+          <span>{plateItem.getScore()}</span>
         </p>
         <p className=" text-[0.55rem] text-gray-500 font-sans font-semibold pr-6 leading-[0.65rem] mt-2">
           {plateItem.getDescription()}
