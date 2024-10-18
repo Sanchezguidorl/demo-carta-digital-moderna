a4f352339f8e0bbb0b6919dce1b4cfca92c274ea
Guido Abel Sanchez <sanchezguidorl@gmail.com>
2024-10-18 19:38:22 -0300

    Fix:Se quita estado negativo de prueba del componente de CategoryItem

--- app/src/infraestructure/components/layouts/categories/CategoryItem.tsx
+++ app/src/infraestructure/components/layouts/categories/CategoryItem.tsx
@@ -17,7 +17,7 @@ function CategoryItem({ categoryItemProps }: { categoryItemProps: CategoryItemTy
         className="overflow-hidden relative flex justify-center items-center h-[110px] sm:h-[200px] md:h-[240px] uppercase brightness-90 hover:brightness-125"
         href={`/pages/category/${categoryItemProps.title.toLowerCase()}/${categoryItemProps.id}`}>
         
-        {!isLoading && <div className='absolute z-30 w-full h-full'><SkeletonDiv /></div>} {/* Mostrar SkeletonDiv mientras se carga */}
+        {isLoading && <div className='absolute z-30 w-full h-full'><SkeletonDiv /></div>} {/* Mostrar SkeletonDiv mientras se carga */}
 
         <Image
           src={categoryItemProps.image}
