c3d77117ef8080da29d74f679a2f39204a5ac9fa
Guido Abel Sanchez <sanchezguidorl@gmail.com>
2024-10-23 23:11:52 -0300

    Se modifica profundidad de botón para ver otras cartas

--- app/src/infraestructure/components/layouts/ShowOtherMenu.tsx
+++ app/src/infraestructure/components/layouts/ShowOtherMenu.tsx
@@ -6,7 +6,7 @@ function ShowOtherMenu() {
       href="/carta-cafeteria.webp" 
       target="_blank" 
       rel="noopener noreferrer" 
-      className='aspect-square w-40 text-sm bg-green-900 text-white rounded-tr-full absolute bottom-0 left-0 flex justify-center items-end font-light p-10 pl-6 border-t border-r hover:cursor-pointer hover:brightness-150 transition-all duration-200'
+      className='aspect-square w-40 text-sm bg-green-900 text-white rounded-tr-full absolute bottom-0 left-0 flex justify-center items-end font-light p-10 pl-6 border-t border-r hover:cursor-pointer hover:brightness-150 transition-all duration-200 z-50'
     >
       <p className="text-center text-shadow">Mirá otro estilo</p>
     </a>
